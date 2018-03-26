8d055466296a495cda3416dffd7267542c345ccc
Imputes <zhuzi@bobbys-air.lan>
2018-03-26 11:31:47 +0800

    Initial Commit

---
+++ Math--Sqrt(x)/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Math--Sqrt(x)
+//
+//  Created by Bobby Negoat on 3/26/18.
+//  Copyright © 2018 Mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
